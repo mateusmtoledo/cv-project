6b55d6da25d53c463b824ec4de135139d360ccd0
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-06-29 20:33:09 -0300

    Add zoom feature to cvPreview component

--- src/components/cvPreview.js
+++ src/components/cvPreview.js
@@ -1,20 +1,41 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import GeneralInformation from "./generalInformation";
 import Education from "./education";
 import WorkExperience from "./workExperience";
 import ScrollContainer from "react-indiana-drag-scroll";
 
 class CvPreview extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scale: 0.6,
+    }
+    this.cvPreviewElement = React.createRef();
+    this.zoom = this.zoom.bind(this);
+  }
+
   componentDidMount() {
     const scroller = document.querySelector('.scroll-container');
     scroller.scrollTop = (scroller.scrollHeight / 4);
     scroller.scrollLeft = (scroller.scrollWidth / 4);
   }
+
+  zoom(event) {
+    const element = this.cvPreviewElement.current;
+    let newScale = this.state.scale;
+    newScale += event.deltaY * -0.002;
+    newScale = Math.min(Math.max(0.3, newScale), 1.185);
+    this.setState({
+      scale: newScale,
+    });
+    element.style.transform = `scale(${newScale})`;
+  }
+
   render() {
     return (
     <ScrollContainer className="scroll-container" ignoreElements="input, button">
-      <div className="cv-preview-container">
-        <div id="cv-preview">
+      <div className="cv-preview-container" onWheel={this.zoom}>
+        <div id="cv-preview" ref={this.cvPreviewElement}>
           <GeneralInformation />
           <Education
             educationInfo={this.props.educationInfo}
